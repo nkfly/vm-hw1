e59fae62b51914c4564e4b3ff83af04ae56772f4
nkfly <nkflyfly@gmail.com>
2015-04-03 22:19:02 +0800

    add ibtc

--- optimization.c
+++ optimization.c
@@ -11,6 +11,7 @@
 
 extern uint8_t *optimization_ret_addr;
 FILE *file_pointer;
+
 /*
  * Shadow Stack
  */
@@ -61,27 +62,51 @@ __thread int update_ibtc;
  * helper_lookup_ibtc()
  *  Look up IBTC. Return next host eip if cache hit or
  *  back-to-dispatcher stub address if cache miss.
+ In function helper_lookup_ibtc, you can get the related cache entry by the argument guest_eip. 
+ If the lookup success, you should return the context pointer of the related translation block. 
+ Otherwise, you can return the optimization_ret_addr to return to the emulation engine.
  */
+struct ibtc_table ibtc;
+target_ulong lasttime_guest_eip
 void *helper_lookup_ibtc(target_ulong guest_eip)
 {
+	int index = guest_eip % IBTC_CACHE_SIZE;
+	lasttime_guest_eip = guest_eip;
+	if (ibtc.htable[index].tb == NULL) {
+		return optimization_ret_addr;	
+	}else if (ibtc.htable[index].tb != NULL && ibtc.htable[index].guest_eip != guest_eip){
+		return optimization_ret_addr;
+	}else if (ibtc.htable[index].tb != NULL && ibtc.htable[index].guest_eip == guest_eip){
+		return ibtc.htable[index].tb;
+	}
     return optimization_ret_addr;
 }
 
 /*
  * update_ibtc_entry()
  *  Populate eip and tb pair in IBTC entry.
+ In function update_ibtc_entry, you should update the translation block and the guest address to the related cache entry.
+  Noticeably that the function helper_lookup_ibtc is executed before the function update_ibtc_entry, 
+  so the information such as guest_ip can be preserved in function helper_lookup_ibtc and used in function update_ibtc_entry later.
  */
 void update_ibtc_entry(TranslationBlock *tb)
 {
+	int index = guest_eip % IBTC_CACHE_SIZE;
+	ibtc.htable[index].tb = tb;
 }
 
 /*
  * ibtc_init()
  *  Create and initialize indirect branch target cache.
+ In function ibtc_init, you need to allocate a space for an ibtc_table. 
+ To ensure the correct of later execution, please set the allocated area to zero.
  */
 static inline void ibtc_init(CPUState *env)
 {
-	print("hey");
+	int i = 0;
+	for (; i < IBTC_CACHE_SIZE;i++){
+		ibtc.htable[i].tb = NULL;
+	}
 }
 
 /*
@@ -92,6 +117,9 @@ static inline void ibtc_init(CPUState *env)
 int init_optimizations(CPUState *env)
 {
     file_pointer = fopen("_host.log","w");
+    char output[100];
+    sprintf(output, "%d", IBTC_CACHE_SIZE);
+    print(output);
     shack_init(env);
     ibtc_init(env);
 
