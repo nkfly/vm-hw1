91cb9ebdd557f633bcab87d61e9a76a6a74b946b
nkfly <nkflyfly@gmail.com>
2015-04-12 18:12:28 +0800

    seem to work

--- optimization.h
+++ optimization.h
@@ -58,7 +58,7 @@ struct shadow_pair_node
     struct shadow_pair_node *next;
 };
 
-void shack_set_shadow(CPUState *env, target_ulong guest_eip, unsigned long *host_eip, TranslationBlock *tb);
+void shack_set_shadow(CPUState *env, target_ulong guest_eip, unsigned long *host_eip);
 inline void insert_unresolved_eip(CPUState *env, target_ulong next_eip, unsigned long *slot);
 unsigned long lookup_shadow_ret_addr(CPUState *env, target_ulong pc);
 void push_shack(CPUState *env, TCGv_ptr cpu_env, target_ulong next_eip);
